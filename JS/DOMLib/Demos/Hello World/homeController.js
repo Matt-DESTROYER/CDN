3c5e763de6a7c17362079702873aa54c8165f969
Matthew James <69026158+Matt-DESTROYER@users.noreply.github.com>
2022-01-05 20:38:06 +1100

    Update homeController.js

--- JS/DOMLib/Demos/Hello World/homeController.js
+++ JS/DOMLib/Demos/Hello World/homeController.js
@@ -1,3 +1,8 @@
-const homeController = APP.Controller();
-homeController.Variables.data = "some data";
-homeController.DOM["title text"].event("click", () => APP.Page = "Hi");
+const homeController = APP.Controller("homeController");
+homeController.CreateVariable("data", "some data", ["out"]);
+const titleText = homeController.DOM("name", "title text");
+titleText.style.cursor = "pointer";
+titleText.addEventListener("click", function() {
+	APP.Page = "Hi";
+	APP.Render();
+});
