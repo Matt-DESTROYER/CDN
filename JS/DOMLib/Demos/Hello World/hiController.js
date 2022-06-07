470abb36b44e891c613d13856879aacd708eba85
Matthew James <69026158+Matt-DESTROYER@users.noreply.github.com>
2022-06-07 13:37:27 +1000

    Update hiController.js

--- JS/DOMLib/Demos/Hello World/hiController.js
+++ JS/DOMLib/Demos/Hello World/hiController.js
@@ -1,7 +1,12 @@
 const hiController = APP.Controller("hiController");
+
 hiController.CreateVariable("user", "Welcome.", ["out"]);
-APP.Input("Enter your name:", ["Submit"]).then((result) => {
-	hiController.SetVariable("user", "Welcome " + result + ".");
-}).catch((error) => {
+
+APP.Input("Enter your name:", ["Submit", "Cancel"]).then(function(result) {
+	if (result.trim()) {
+		hiController.SetVariable("user", "Welcome " + result + ".");
+		hiController.DOM("title").textContent = "Hello " + result + "!";
+	}
+}).catch(function(error) {
 	console.warn("Could not get input: " + error);
 });
