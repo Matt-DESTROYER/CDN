c89d5d6a52b3dceba0d54474bdaeb5463d9b51a1
Matthew James <69026158+Matt-DESTROYER@users.noreply.github.com>
2023-12-04 13:28:52 +1100

    Open source Tokeniser.js

--- JS/Tokeniser/Tokeniser.js
+++ JS/Tokeniser/Tokeniser.js
@@ -1,1 +1,75 @@
-const Tokeniser=(function(){const t=/-?(\d+\.d+|\d+\.|\.\d+|\d+)((e|E)(\+|-)?\d+)?/g;return{settings:{operators:["<",">","=","+","-","*","/","?","!"],separators:[",",".",";",":"," ","\t","\n"],groupers:["(",")","[","]","{","}",'"','"',"'","'"],keepWhiteSpacesAsTokens:!1,trimTokens:!0},isNumber:function(e){return"number"==typeof e||"string"==typeof e&&t.test(e)},closeGrouper:function(t){return this.settings.groupers.includes(t)?this.settings.groupers[this.settings.groupers.indexOf(t)+1]:null},tokenType:function(t){return this.settings.operators.includes(t)?"operator":this.settings.separators.includes(t)?"separator":this.settings.groupers.includes(t)?"grouper":"other"},parseString:function(t){if("string"!=typeof t){if(null===t)return"null";t="object"==typeof t?JSON.stringify(t):t.toString()}let e=[],s="";for(let i=0;i<t.length;i++)this.tokenType(s)!==this.tokenType(t[i])||"separator"===this.tokenType(t[i])?(""!==s.trim()?e.push(this.settings.trimTokens?s.trim():s):this.settings.keepWhiteSpacesAsTokens&&e.push(s),s=t[i],"separator"===this.tokenType(s)&&(""!==s.trim()?e.push(this.settings.trimTokens?s.trim():s):this.settings.keepWhiteSpacesAsTokens&&e.push(s),s="")):s+=t[i];return""!==s.trim()?e.push(this.settings.trimTokens?s.trim():s):this.settings.keepWhiteSpacesAsTokens&&e.push(s),e.filter(t=>""!==t)}}})();console.log("Loaded Tokeniser.js by Matthew James");
+const Tokeniser = (function () {
+	const numberRegex = /-?(\d+\.d+|\d+\.|\.\d+|\d+)((e|E)(\+|-)?\d+)?/g;
+	return {
+		settings: {
+			operators: ["<", ">", "=", "+", "-", "*", "/", "?", "!"],
+			separators: [",", ".", ";", ":", " ", "\t", "\n"],
+			groupers: ["(", ")", "[", "]", "{", "}", '"', '"', "'", "'"],
+			keepWhiteSpacesAsTokens: false,
+			trimTokens: true
+		},
+		isNumber: function (value) {
+			if (typeof value === "number") {
+				return true;
+			} else if (typeof value === "string") {
+				return numberRegex.test(value);
+			}
+			return false;
+		},
+		closeGrouper: function (grouper) {
+			if (this.settings.groupers.includes(grouper)) {
+				return this.settings.groupers[this.settings.groupers.indexOf(grouper) + 1];
+			}
+			return null;
+		},
+		tokenType: function (char) {
+			if (this.settings.operators.includes(char)) {
+				return "operator";
+			} else if (this.settings.separators.includes(char)) {
+				return "separator";
+			} else if (this.settings.groupers.includes(char)) {
+				return "grouper";
+			}
+			return "other";
+		},
+		parseString: function (str) {
+			if (typeof str !== "string") {
+				if (str === null) {
+					return "null";
+				} if (typeof str === "object") {
+					str = JSON.stringify(str);
+				} else {
+					str = str.toString();
+				}
+			}
+			let tokens = [], _tempToken = "";
+			for (let i = 0; i < str.length; i++) {
+				if (this.tokenType(_tempToken) !== this.tokenType(str[i]) || this.tokenType(str[i]) === "separator") {
+					if (_tempToken.trim() !== "") {
+						tokens.push(this.settings.trimTokens ? _tempToken.trim() : _tempToken);
+					} else if (this.settings.keepWhiteSpacesAsTokens) {
+						tokens.push(_tempToken);
+					}
+					_tempToken = str[i];
+					if (this.tokenType(_tempToken) === "separator") {
+						if (_tempToken.trim() !== "") {
+							tokens.push(this.settings.trimTokens ? _tempToken.trim() : _tempToken);
+						} else if (this.settings.keepWhiteSpacesAsTokens) {
+							tokens.push(_tempToken);
+						}
+						_tempToken = "";
+					}
+				} else {
+					_tempToken += str[i];
+				}
+			}
+			if (_tempToken.trim() !== "") {
+				tokens.push(this.settings.trimTokens ? _tempToken.trim() : _tempToken);
+			} else if (this.settings.keepWhiteSpacesAsTokens) {
+				tokens.push(_tempToken);
+			}
+			return tokens.filter((token) => token !== "");
+		}
+	};
+})();
+console.log("Loaded Tokeniser.js by Matthew James");
